92467a5aaf1b4d0f6a18e181010b0fc72d45c00b
ricardousp <ricardoramos.usp@gmail.com>
2021-03-09 17:01:57 -0300

    Adicionando o projeto de CRUD em PHP com Banco de Dados

---
+++ CRUD/cadastrar.php
@@ -0,0 +1,107 @@
+<?php
+
+$conn = mysqli_connect('localhost', 'root', '', 'relacionamentos');
+if (mysqli_connect_errno()) {
+    die('Não foi possível se conectar com o banco de dados: ' . mysqli_connect_error());
+}
+
+$msg = array();
+
+try 
+{
+    if ($_POST)
+    {
+        // PHP 8 = Aceita expressões throw num operador ternário / binário
+        // FILTER_FLAG_NO_ENCODE_QUOTES muda o comportamento do filtro FILTER_SANITIZE_STRING
+        // Aplica tudo o que está no FILTER_SANITIZE_STRING menos a codificação de QUOTES (aspas)
+        $nome = filter_var($_POST['nome'], FILTER_SANITIZE_STRING, FILTER_FLAG_NO_ENCODE_QUOTES) ?: throw new Exception('Por favor, preencha o campo Nome!');
+        $email = filter_var($_POST['email'], FILTER_VALIDATE_EMAIL) ?: throw new Exception('E-mail inválido!');
+        // Considera 1 se o dado foi enviado e 0 caso contrário
+        $status = isset($_POST['status']) ? 1 : 0;
+
+        //var_dump($nome); - Remover o FILTER_FLAG_NO_ENCODE_QUOTES
+        //exit;
+
+        // mysqli_real_escape_string() trata (coloca o escape nas aspas) todas as aspas dentro da string para não dar problema na instrução sql
+        // Se remover o mysqli_real_escape_string(), entende que o apostrofo do nome fecha/encerra a consulta sql
+        // É recomendado usar o mysqli_real_escape_string() com todas as strings mesmo quando não esperamos aspas
+        $nome = mysqli_real_escape_string($conn, $nome);
+        $email = mysqli_real_escape_string($conn, $email);
+        $sql = "INSERT INTO clientes (nome, email, status) VALUES('$nome', '$email', $status)";
+
+        //print $sql;
+
+        $resultado = mysqli_query($conn, $sql);
+
+        // mysqli_errno() retorna o número do erro da operação (SELECT, INSERT, UPDATE e DELETE) executada dentro do banco de dados
+        if ($resultado === false || mysqli_errno($conn)) {
+            throw new Exception('Erro ao realizar operação no banco de dados: ' . mysqli_error($conn));
+        }
+
+        // operação de inserção na base
+        $msg = array(
+            'classe' => 'alert-success',
+            'mensagem' => 'Cliente cadastrado com sucesso!'
+        );
+    }
+}
+catch(Exception $ex)
+{
+    $msg = array(
+        'classe' => 'alert-danger',
+        'mensagem' => $ex->getMessage()
+    );
+}
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+    <title>Clientes | Cadastro</title>
+</head>
+
+<body>
+
+    <div class="container py-5">
+        <h1>
+            Clientes
+            <a href="listagem.php" class="btn btn-success float-right">Listagem</a>
+        </h1>
+        <p>Use o formulário abaixo para cadastrar um cliente:</p>
+
+        <!-- Se tiver alguma msg de erro para ser exibida -->
+        <?php if ($msg) : ?>
+            <div class="alert <?= $msg['classe'] ?>">
+                <?= $msg['mensagem']; ?>
+            </div>
+        <?php endif; ?>
+
+        <form method="POST">
+            <div class="form-group">
+                <!-- O atributo for é usado em labels. Refere-se ao id do elemento ao qual este label está associado. -->
+                <label for="nome">Nome:</label>
+                <input type="text" name="nome" id="nome" class="form-control" value="<?= $_POST['nome'] ?? '' ?>">
+            </div>
+            <div class="form-group">
+                <label for="email">E-mail:</label>
+                <input type="email" name="email" id="email" class="form-control" value="<?= $_POST['email'] ?? '' ?>">
+            </div>
+            <div class="form-group">
+                <label for="status">Status:</label> <br>
+                <input type="checkbox" name="status" id="status" checked /> Ativo
+            </div>
+            <div class="form-group">
+                <button type="submit" class="btn btn-primary">
+                    Cadastrar
+                </button>
+            </div>
+        </form>
+    </div>
+
+</body>
+
+</html>
---
+++ CRUD/listagem.php
@@ -0,0 +1,152 @@
+<?php
+
+// IMPORTANTE: Sempre validar as informações antes de passar para a instrução sql
+// Exemplo de SQL Injection
+//$nome = "Ricardo%' OR '%";
+//$nome = "Ricardo%'; DROP TABLE clientes; #"; // # comenta tudo o que vem depois para não dar erro de sintax no sql.
+//"SELECT * FROM clientes WHERE nome LIKE '%Ricardo%'; DROP TABLE clientes; #%'";
+
+$conn = mysqli_connect('localhost', 'root', '', 'relacionamentos');
+// Todas as operaçoes realizadas aqui utilizarão a codificação utf-8
+mysqli_set_charset($conn, 'utf-8');
+if(mysqli_connect_errno()){
+    die('Não foi possível se conectar com o banco de dados: ' . mysqli_connect_error());
+}
+
+
+$msg = array();
+
+$sql_busca = "SELECT * FROM clientes";
+
+try 
+{
+    // Verifica se tem dados via GET e existi o dado excluir
+    if ($_GET && isset($_GET['excluir'])){
+
+        // Se usar apenas o $id = $_GET['excluir']; ocasiona falha de SQL Injection
+        $id = filter_var($_GET['excluir'], FILTER_VALIDATE_INT);
+
+        if($id === false){
+            throw new Exception("ID inválido para exclusão");
+        }
+
+        // Exemplo de SQL Injection - $sql = "DELETE FROM clientes WHERE cliente_id = 2 OR 1 = 1";
+        // Exemplo de SQL Injection - $sql = "DELETE FROM clientes WHERE cliente_id = 2; DROP TABLE clientes; se for um usuário com permissão de root";
+        $sql = "DELETE FROM clientes WHERE cliente_id = $id";
+        $resultado = mysqli_query($conn, $sql);
+
+        if ($resultado === false || mysqli_errno($conn)) {
+            throw new Exception('Erro ao realizar a exclusão no banco de dados: ' . mysqli_error($conn));
+        }
+
+         // operação de exclusão na base
+         $msg = array(
+            'classe' => 'alert-success',
+            'mensagem' => 'Cliente excluído com sucesso!'
+        );
+    }
+    
+    // Verifica se tem alguma informação dentro do GET e se está setado na url o parâmetro busca
+    if ($_GET && isset($_GET['busca']))
+    {
+        $termo = filter_var($_GET['busca'], FILTER_SANITIZE_STRING, FILTER_FLAG_NO_ENCODE_QUOTES);
+        if (!empty($termo)) {
+            $sql_busca = "SELECT * FROM clientes WHERE nome LIKE '%$termo%'";
+        }
+    }
+    
+}
+catch(Exception $ex)
+{
+    $msg = array(
+        'classe' => 'alert-danger',
+        'mensagem' => $ex->getMessage()
+    );
+}
+finally {
+    // A função mysqli_query() executa a query no banco a partir da conexão fornecida.
+    $resultado = mysqli_query($conn, $sql_busca);
+    // Se retornou algum dado na consulta
+    if ($resultado) {
+         // mysqli_fetch_all() pega todos os resultados e cria um array (matriz)
+        // Cada resultado é um array associativo
+        // MYSQLI_NUM é o padrão numérico
+        $lista_clientes = mysqli_fetch_all($resultado, MYSQLI_ASSOC);
+    }
+}
+
+// Se a estrutura da tabela mudar a posição dos indícies da matriz também mudará
+// print '<pre>';
+// print_r($lista_clientes);
+// print '</pre>';
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+    <title>Clientes</title>
+</head>
+<body>
+
+    <div class="container py-5">
+        <h1>
+            Clientes
+            <a href="cadastrar.php" class="btn btn-success float-right">Cadastrar</a>
+        </h1>
+        <p>Veja na listagem abaixo os clientes cadastrados:</p>
+        
+        <!-- Se tiver alguma msg de erro para ser exibida -->
+        <?php if ($msg) : ?>
+            <div class="alert <?= $msg['classe'] ?>">
+                <?= $msg['mensagem']; ?>
+            </div>
+        <?php endif; ?>
+
+        <form method="GET">
+            <div class="row">
+                <div class="form-group col-md-10">
+                    <input type="search" name="busca" class="form-control" value="<?= $termo ?? '' ?>" placeholder="Buscar cliente por nome" />
+                </div>
+                <div class="form-group col-md-2">
+                    <button class="btn btn-primary w-100">
+                        Buscar
+                    </button>
+                </div>
+            </div>
+        </form>
+
+        <table class="table table-bordered">
+            <thead>
+                <tr>
+                    <th>Id</th>
+                    <th>Nome</th>
+                    <th>E-mail</th>
+                    <th>Status</th>
+                    <th colspan="2"></th>
+                </tr>
+            </thead>
+            <tbody>
+                <?php foreach ($lista_clientes as $cliente) : ?>
+                    <tr>
+                        <td><?= $cliente['cliente_id'] ?></td>
+                        <td><?= $cliente['nome'] ?></td>
+                        <td><?= $cliente['email'] ?></td>
+                        <td><?= ($cliente['status']) ? 'ATIVO' : 'INATIVO' ?></td>
+                        <td>
+                            <a href="editar.php?id=<?= $cliente['cliente_id'] ?>" class="btn btn-primary">Editar</a>
+                        </td>
+                        <td>
+                            <a href="listagem.php?excluir=<?= $cliente['cliente_id'] ?>" class="btn btn-danger">Excluir</a>
+                        </td>
+                    </tr>
+                <?php endforeach; ?>
+            </tbody>
+        </table>
+    </div>
+</body>
+</html>
