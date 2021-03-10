968ff7141bb854ff65bd76f541db3b69c87adb52
Ricardo Ramos de Oliveira <ricardoramos.usp@gmail.com>
2021-03-10 15:07:46 -0300

    Update script.sql

--- CRUD/script.sql
+++ CRUD/script.sql
@@ -1,10 +1,11 @@
 create database crud;
 
 CREATE TABLE `clientes` (
-  `cliente_id` int NOT NULL PRIMARY KEY,
+  `cliente_id` int NOT NULL AUTO_INCREMENT,
   `nome` varchar(50) NOT NULL,
   `email` varchar(50) NOT NULL,
-  `status` tinyint(1) DEFAULT '1'
+  `status` tinyint(1) DEFAULT '1',
+   PRIMARY KEY (cliente_id)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 INSERT INTO `clientes` (`cliente_id`, `nome`, `email`, `status`) VALUES
